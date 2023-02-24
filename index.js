5cb7c7d568d5b75240f6a62919752c96af750886
iraprojects <kristenirazu@gmail.com>
2023-02-23 21:21:31 -0600

    Updates

---
+++ index.js
@@ -0,0 +1,49 @@
+import validator from "./validator.js";
+
+//No ejecuta codigo JS hasta que HTML cargue por completo. ej Js comillas simples
+document.addEventListener('DOMContentLoaded', function () {
+  const creditNumber = document.querySelector('#creditNumber');
+  const btn_validation = document.querySelector('#btn_validation');
+  const p_creditNumber = document.querySelector('#p_credit-number');
+  const p_numberValid = document.querySelector('#p_number-valid');
+  const p_emptyInput = document.querySelector('#p_emptyInput');
+  const showCard = document.querySelector('#showCard');
+
+  //Obtiene el numero de la tarjeta.
+  btn_validation.addEventListener('click', function () {
+    p_creditNumber.innerHTML = '';
+    p_numberValid.innerHTML = '';
+    p_emptyInput.innerHTML = '';
+    showCard.style.display = 'none';
+    if (creditNumber.value === '' || creditNumber.value === ' ') {
+      p_emptyInput.innerHTML = 'Please, enter a card number';
+    } else {
+      showCard.style.display = 'block';
+      p_creditNumber.innerHTML = validator.maskify(creditNumber.value);
+      if (validator.isValid(creditNumber.value) === true) {
+        p_numberValid.innerHTML = 'is Valid √';
+        p_numberValid.style.background = 'linear-gradient(90deg, #1a8b17 14%, #74e386 100%)';
+        showCard.style.background = 'linear-gradient(90deg, #335db5 14%, #00ff88 100%)';
+      } else {
+        p_numberValid.innerHTML = 'Not Valid ☓';
+        p_numberValid.style.background = 'linear-gradient(90deg, #9f1e1e 14%, #e34864 100%)';
+        showCard.style.background = 'linear-gradient(90deg, #bebebe 14%, #b5334a 100%)';
+      }
+    }
+  });
+
+  //detecta solo numeros al teclear
+  creditNumber.addEventListener('keydown', (event) => {
+    const keyValue = event.key;
+    const regex = new RegExp(/^[0-9]+$/);
+    let test = regex.test(keyValue);
+    //console.log(keyValue);
+    if (keyValue === 'Backspace') {
+      test = true;
+    }
+    if (!test) {
+      event.preventDefault();
+    }
+    //console.log(test);
+  });
+});
---
+++ validator.js
@@ -0,0 +1,75 @@
+const validator = {
+  isValid: function (creditNumber) {
+    let creditNumberInvert = "";
+    let creditNumberFinal = "";
+    let totalSum = 0;
+    let stringConvert = "";
+    let multiple = 0;
+    for (let i = creditNumber.length - 1; i >= 0; i--) {
+      creditNumberInvert += creditNumber[i];
+    }
+    for (let i = 0; i <= creditNumberInvert.length - 1; i++) {
+      if (i % 2 === 1) {
+        multiple = Number(creditNumberInvert[i] * 2);
+        if (multiple >= 10) {
+          stringConvert = multiple.toString();
+          creditNumberFinal +=
+            Number(stringConvert[0]) + Number(stringConvert[1]);
+          totalSum += Number(creditNumberFinal[i]);
+        } else {
+          creditNumberFinal += multiple;
+          totalSum += Number(creditNumberFinal[i]);
+        }
+      } else {
+        creditNumberFinal += creditNumberInvert[i];
+        totalSum += Number(creditNumberFinal[i]);
+      }
+    }
+    if (totalSum % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  maskify: function (creditNumber) {
+    let creditNumberMaskify = "";
+    for (let i = 0; i < creditNumber.length; i++) {
+      if (i < creditNumber.length - 4) {
+        creditNumberMaskify += "#";
+      } else {
+        creditNumberMaskify += creditNumber[i];
+      }
+    }
+    return creditNumberMaskify;
+  },
+  /*
+  showBank: function (creditNumber) {
+    
+    const inventory = [
+    { name: "JCB", quantity: 1800 },
+    { name: "Diner's Club", quantity: 2014 },
+    { name: "American Express", quantity: 34 },
+    { name: "JCB", quantity: 305 },
+    ];
+    //sacar los primeros cuatro numeros
+    //convertirlos a number
+    const numbers = "34564365676434244242"
+    const a = (numbers.slice(0,4))
+    const two = Number(a.slice(0,-2)) //corta dos ultimos elementos
+    const tw2 = Number(a.slice(0,-1)) //corta ultimo elemento
+    const result = inventory.find(({ quantity }) => quantity === Number(a.slice(0,-2)));
+    //console.log(result.name);
+    //console.log(a)
+    //console.log(two)
+    //console.log(tw2)
+    //console.log(fournumber.includes(305));
+    if(result == undefined){
+      console.log("Fallo")
+    }else{
+      console.log(result.name)
+    }
+    
+  },*/
+};
+export default validator;
